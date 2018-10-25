3e490019fd1a31f3dedbb6f1dca5cfb01e9288e5
Nep67 <nickpipino67@gmail.com>
2018-10-25 16:52:42 -0400

    Initial commit

---
+++ src/main/java/edu/ccsu/datamail/DataMailApplication.java
@@ -0,0 +1,12 @@
+package edu.ccsu.datamail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMailApplication.class, args);
+	}
+}
